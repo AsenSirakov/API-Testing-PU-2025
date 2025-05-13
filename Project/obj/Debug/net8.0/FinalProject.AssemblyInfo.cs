7f921e44bcaef6d380a4d186b1a3f7fb2f099803
Asen Sirakov <asko2040@abv.bg>
2025-05-13 15:41:34 +0300

    Commit

--- Project/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ Project/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+307dbe44f643338256323c7694a73d575f1d8a11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4393c4e14ed0d80db8d0c8f1b8a725f8dd0564e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
